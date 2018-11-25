fd34689efcd93667a45416df672ee77601b595f5
MCPE_PC <maxpjh0528@naver.com>
2018-11-25 17:39:49 +0900

    Oh really

--- jsdoc.sh
+++ jsdoc.sh
@@ -2,6 +2,7 @@
 if [[ "$TRAVIS_BRANCH" == 'master' ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
 	git config --global user.name 'MCPE_PC'
 	git config --global user.email 'maxpjh0528@naver.com'
+	git config --global credential.helper store
 
 	grunt jsdoc &&\
 	echo "https://$GITHUB_TOKEN:x-oauth-basic@github.com" >> ~/.git-credentials &&\
