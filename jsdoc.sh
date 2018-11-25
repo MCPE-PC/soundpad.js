257561578ab9a7cfd9d7ddafca480a217f6c45c1
MCPE_PC <maxpjh0528@naver.com>
2018-11-25 18:21:03 +0900

    @_%

--- jsdoc.sh
+++ jsdoc.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 if [[ "$TRAVIS_BRANCH" == 'master' ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
-	git config --global user.name 'MCPE_PC'
 	git config --global user.email 'maxpjh0528@naver.com'
+	git config --global user.name 'MCPE_PC'
 	git config --global credential.helper store
+	echo "https://$GITHUB_TOKEN:x-oauth-basic@github.com" >> ~/.git-credentials
 
 	grunt jsdoc &&\
-	echo "https://$GITHUB_TOKEN:x-oauth-basic@github.com" >> ~/.git-credentials &&\
+	cd docs &&\
+	mv ../.git ./ &&\
+	git checkout gh-pages
 	git add docs &&\
-	git commit -m "Travis JSDoc automation: build $TRAVIS_BUILD_ID" &&\
-	git push -u origin master &&\
+	git commit -m "Travis JSDoc automation: build #$TRAVIS_BUILD_NUMBER" &&\
+	git push -u origin gh-pages &&\
 	exit 0
 fi
 
