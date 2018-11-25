2ac4a16596a0d9f8913691daa669215b5b46879c
MCPE_PC <maxpjh0528@naver.com>
2018-11-25 22:40:14 +0900

    Not-a-commit

--- jsdoc.sh
+++ jsdoc.sh
@@ -9,8 +9,9 @@ if [[ "$TRAVIS_BRANCH" == 'master' ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]
 	cd docs &&\
 	mv ../.git ./ &&\
 	git checkout -b gh-pages &&\
+	echo 'soundpad.js.org' > CNAME
 	git add . &&\
-	git commit -m "Travis JSDoc automation: build #$TRAVIS_BUILD_NUMBER" > /dev/null &&\
+	git commit -m "Travis CI + JSDoc: build #$TRAVIS_BUILD_NUMBER" > /dev/null &&\
 	git push -u origin +gh-pages &&\
 	exit 0
 fi
