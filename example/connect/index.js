a2b637de097ed6c3d6b96bcc112b82043f1d1b75
MCPE_PC <maxpjh0528@naver.com>
2019-04-03 15:19:30 +0000

    JSDoc Automation with Travis CI: build #34

--- example/connect/index.js
+++
@@ -1,8 +0,0 @@
-const Soundpad = require('../..');
-
-const soundpad = new Soundpad();
-console.log('Connecting to Soundpad...');
-soundpad.connect().then(() => {
-	console.log('Connected to Soundpad');
-	soundpad.disconnect();
-});
--- index.js
+++
@@ -1,8 +0,0 @@
-require('@babel/register');
-
-if (process.platform !== 'win32' && !process.env.FORCE) {
-	throw new Error('This module only works on Windows');
-}
-
-module.exports = require('./src');
-module.exports.Caller = require('./src/caller');
